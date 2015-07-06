065213256dd39b9b4758ed7e80a6bfc9dfdfbcc2
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-06 23:55:39 +0530

    solution 1.37 recurssive update

--- solution_137_1.scm
+++ solution_137_1.scm
@@ -1,21 +1,23 @@
-(define (cont-frac  n d  k)
-	(if(equal? k 0)
-	(/ n d)
-	(/ n (+ d (cont-frac n d (- k 1))))))
+(define (num x) 1.0)
+(define (deno x) 1.0)
+
+
+(define (cont-frac nump denop k start)
+	(if(equal? start k)
+	(/ (nump start) (denop start))
+	(/ (nump start) (+ (denop start) (cont-frac nump denop  k (+ start 1))))))
 
 
 (define tolerance 0.00001)
 (define value 0.618046971)
-	(define (fixed-point f first-guess n d )
-	(define (close-enough? v1 v2)
-		(< (abs (- v1 v2)) tolerance))
-	(define (try guess)
-		;(let (next (f n d guess)
-		(if (close-enough? value (f n d guess))
-			guess
-			(try (+ guess 1))))
- 	(try first-guess))
-
-
+(define (fixed-point f first-guess numx denox )
+(define (close-enough? v1 v2)
+	(< (abs (- v1 v2)) tolerance))
+(define (try guess)
+	;(let (next (f n d guess)
+	(if (close-enough? value (f numx denox guess 0))
+		guess
+		(try (+ guess 1))))
+	(try first-guess))
 
 
