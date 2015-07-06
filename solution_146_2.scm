474026a1b5a1a23f396c4de530e05acf27bd7252
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-06 18:38:50 +0530

    solutions to 1.46

---
+++ solution_146_2.scm
@@ -0,0 +1,25 @@
+
+
+
+(define (iterative-improve num good-enough? improve-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) 0.00001))
+ 
+  (define (AVERAGE X Y) 
+          (/ (+ X Y) 2))
+   
+  (define (improve guess x)
+      (average guess (/ x guess)))
+   
+  (define  (SQUARE X) (* X X))
+  
+ (lambda (x)
+    
+    (if(close-enough? x (improve x num) )
+         (improve x num)
+         (((iterative-improve num close-enough? improve) (improve x num))
+         )))
+ 
+(define (sqrt x)
+  	;(lambda (y)
+	     ((iterative-improve x close-enough? improve) 1.0)))
