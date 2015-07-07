7c3c9200d09b1c93364bf2662b9cd982ba7cb4c2
Lakshadeep <lakshadeep.naik@gmail.com>
2015-07-07 12:36:07 +0530

    solution to 2.1 updated

--- Chapter_2/solution_21.scm
+++ Chapter_2/solution_21.scm
@@ -1,10 +1,19 @@
 (define (make-test n d) 
+  (let ((g (gcd n d)))
   (if (and (< n 0) (< d 0))
-  (cons (* n -1) (* d -1))
+  (cons (/ (* n -1) g) (/ (* d -1) g))
   (if(< d 0)
-      (cons (* n -1) (* d -1))
-      (cons n d))))
+      (cons (/ ( * n -1) g) ( / (* d -1) g))
+      (cons n d)))))
 
 (define (numer x) (car x))
 
 (define (denom x) (cdr x))
+
+
+(define (print-rat x)
+	(newline)
+ 	(display (numer x))
+    (display "/")
+    (display(denom x)))  
+  
